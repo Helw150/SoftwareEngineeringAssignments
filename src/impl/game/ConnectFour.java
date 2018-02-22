593c82da0e2d323f515ac640649b8ea2e2092e8c
Helw150 <wbh230@nyu.edu>
2018-02-22 23:22:03 +0400

    Final Polish and Comments

--- src/impl/game/ConnectFour.java
+++ src/impl/game/ConnectFour.java
@@ -21,18 +21,23 @@ public class ConnectFour extends Game {
     public ConnectFour(){
 	this(6, 7);
     }
-    
+
+    // Take numRows and numColumns
     public ConnectFour(int numRows, int numCols){
 	this.numRows = numRows;
 	this.numCols = numCols;
+	// Game currently only handles connect 4
 	this.connectToWin = 4;
 	this.numPlayers = 2;
+	// Set the initial user
 	this.currentPlayer = 0;
 	this.winningPlayer = Chip.EMPTY;
 	this.board = new Chip[this.numRows][this.numCols];
 	this.players = new Chip[this.numPlayers];
+	// Set player chip colors
 	this.players[0] = Chip.RED;
 	this.players[1] = Chip.BLUE;
+	// Setup the board as empty
 	for(int i = 0; i < numRows; i++){
 	    for(int j = 0; j < numCols; j++){
 		board[i][j] = Chip.EMPTY;
@@ -40,14 +45,17 @@ public class ConnectFour extends Game {
 	}
     }
 
+    // Simple getter for rows
     public int getRows(){
 	return this.numRows;
     }
 
+    // Simple getter for columns
     public int getColumns(){
 	return this.numCols;
     }
 
+    // Check if the chip is valid and then return its val if so
     public Chip getChip(int row, int col){
 	if (row < 0 || col < 0 || this.numRows <= row || this.numCols <= col){
 	    throw new GameIndexOutOfBoundsException(row, col);
@@ -56,6 +64,7 @@ public class ConnectFour extends Game {
 	}
     }
 
+    // Check if the indices are valid and then call my internal place function that deals with just column
     public void placeChip(int row, int col) throws GameStateException {
 	if(row != 0 || this.numCols <= col || col < 0 || this.board[0][col] != Chip.EMPTY){
 	    throw new GameIndexOutOfBoundsException(0, col);
@@ -65,27 +74,33 @@ public class ConnectFour extends Game {
     }
 
 
+    // Check if the game is over, otherwise simulate placed piece
     public void placeChip(int col) throws GameStateException {
-	int row = 0;
-	for(int i = 0; i < numRows; i++){
-	    if(board[i][col] == Chip.EMPTY){
-		row = i;
-	    }
-	}
-
 	if(this.isGameOver()) {
 	    throw new GameStateException();
 	} else {
+	    // Simulates the "falling" of the piece
+	    int row = 0;
+	    for(int i = 0; i < numRows; i++){
+		if(board[i][col] == Chip.EMPTY){
+		    row = i;
+		}
+	    }
+	    // Set the requested piece to the current players color
 	    this.board[row][col] = this.players[this.currentPlayer];
+	    // If the game isn't over iterate to the next player
 	    if(!this.isGameOver()){
 		this.currentPlayer = (this.currentPlayer+1)%this.numPlayers;
 	    }
+	    // Inform the observable that it has changed
 	    setChanged();
 	}
+	// Let observers know that everything has changed
 	notifyObservers();
     }
 
 
+    // A getter that simply confirms the game is over first and excepts if it isnt over
     public Chip getWinningPlayer() throws GameStateException{
 	if (this.isGameOver()){
 	    return this.winningPlayer;
@@ -94,6 +109,7 @@ public class ConnectFour extends Game {
 	}
     }
 
+    // If the game is over returns empty. Otherwise, simply a getter.
     public Chip getCurrentPlayer(){
 	if(this.isGameOver()){
 	    return Chip.EMPTY;
@@ -101,7 +117,8 @@ public class ConnectFour extends Game {
 	    return this.players[this.currentPlayer];
 	}
     }
-    
+
+    // Check the four spaces to the right for a win if it is valid
     public boolean horizontalWin(int i, int j, Chip player){
 	if(j < numCols-(connectToWin-1)){
 	    return (this.board[i][j] == player && this.board[i][j+1] == player && this.board[i][j+2] == player && this.board[i][j+3] == player);
@@ -109,7 +126,8 @@ public class ConnectFour extends Game {
 	    return false;
 	}
     }
-
+    
+    // Check the fours spaces below for a win if it is valid
     public boolean verticalWin(int i, int j, Chip player){
 	if(i < numRows-(connectToWin-1)){
 	    return(this.board[i][j] == player && this.board[i+1][j] == player && this.board[i+2][j] == player && this.board[i+3][j] == player);
@@ -118,6 +136,7 @@ public class ConnectFour extends Game {
 	}
     }
 
+    // Check from top left to bottom right for a diagonal win
     public boolean top2bottomDiagonalWin(int i, int j, Chip player){
 	if(3 <= i && 3 <= j){
 	    return (this.board[i][j] == player && this.board[i-1][j-1] == player && this.board[i-2][j-2] == player && this.board[i-3][j-3] == player);
@@ -126,6 +145,7 @@ public class ConnectFour extends Game {
 	}
     }
 
+    // Check from bottom left to top right for a diagonal win
     public boolean bottom2topDiagonalWin(int i, int j, Chip player){
 	if(3 <= i && j < numCols-(connectToWin-1)){
 	    return (this.board[i][j] == player && this.board[i-1][j+1] == player && this.board[i-2][j+2] == player && this.board[i-3][j+3] == player);
@@ -134,22 +154,30 @@ public class ConnectFour extends Game {
 	}
     }
 
+    // Check all my win conditions
     public boolean isGameOver(){
+	// Check every win condition in a single loop
 	for (int i = 0; i < this.numRows; i++){
 	    for(int j = 0; j < this.numCols; j++){
+		// Check for wins on each player
 		for(Chip player : this.players){
+		    // Check all booleans - the validity of the space as a check spot is contained internally
 		    if(horizontalWin(i,j,player) || verticalWin(i,j,player) || top2bottomDiagonalWin(i,j,player) || bottom2topDiagonalWin(i,j,player)){
+			// if there is a win set my winning player to which of the two was found
 			this.winningPlayer = player;
 			return true;
 		    }
 		}
 	    }
 	}
+	// Check that there are empty spaces in the board
 	for (int i = 0; i < this.numCols; i++){
+	    // We only need to check the top row due to falling
 	    if(board[0][i] == Chip.EMPTY){
 		return false;
 	    }
 	}
+	// If the board was full and reached this point, the game is a tie.
 	this.winningPlayer = Chip.EMPTY;
 	return true;
     }
--- src/impl/view/Console.java
+++ src/impl/view/Console.java
@@ -12,12 +12,14 @@ import exc.GameIndexOutOfBoundsException;
     
 public class Console extends View {
     private Game game;
-    
+
+    // Add this console using aggregate method to the game given to it as an Observer
     public Console(Game game){
 	this.game = game;
 	this.game.addObserver(this);
     }
-    
+
+    // Render the whole game using some basic dividers and the Enum name
     public void render(Game game){
 	System.out.println("===========================");
 	for(int i = 0; i < game.getRows(); i++){
@@ -29,25 +31,34 @@ public class Console extends View {
 	}
     }
 
+    // Take User Input from the Game
     public void play(Game game){
-	System.out.print("Select a Column to Play: ");
-	Scanner stdin = new Scanner(System.in);
 	try {
+	    if (game.getCurrentPlayer() != Chip.EMPTY) {
+	    System.out.println(game.getCurrentPlayer() +" player - select a Column to Play: ");
+	    } else {
+		System.out.println("Game Over - Enter a number(0-6) and hit enter to announce the winner.");
+	    }
+	    Scanner stdin = new Scanner(System.in);
 	    int col = Integer.parseInt(stdin.next());
 	    game.placeChip(0, col);
 	}
+	// This should only occur if the game is appropriately over, so break and move to the higher level announcement
 	catch (GameStateException e) {
-	    System.out.println("Invalid play given the state of the game.");
+	    return;
 	} catch (GameIndexOutOfBoundsException e){
 	    System.out.println("Invalid Range for move.");
-	    play(this.game);
+	    // Give the user the choice to play again
+	    play(game);
 	} catch (NumberFormatException e){
 	    System.out.println("Please input a number.");
-	    play(this.game);
+	    // Give the user the choice to play again
+	    play(game);
 	}
       
     }
 
+    // Render the updated board and give the user another option to play
     public void update(Observable o, Object game) {
         render(this.game);
 	play(this.game);
